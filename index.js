b0ec19ee2f7751d5e93bbfb2064b2e7625392ab0
Wallace Middleton <wallace@codecultivators.com>
2023-04-14 12:33:43 -0400

    refactor swagger config to separate file

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const config = require('dotenv').config(),
 express = require('express'),
 bodyParser = require('body-parser'),
 routes = require('./routes'),
-swaggerJsdoc = require("swagger-jsdoc"),
-swaggerUi = require("swagger-ui-express");
+swaggerConfig = require('./swaggerConfig');
 
 const app = express();
 
@@ -15,40 +14,7 @@ app.use(
 )
 
 routes(app);
-
-const options = {
-  definition: {
-    openapi: "3.0.0",
-    info: {
-      title: "Code Academy Portfolio Project E-Commerce API",
-      version: "0.1.0",
-      description:
-        "This is simple E-Commerce API using the PERN stack",
-      license: {
-        name: "MIT",
-        url: "https://spdx.org/licenses/MIT.html",
-      },
-      contact: {
-        name: "Support",
-        url: "https://example.com",
-        email: "support@example.com",
-      },
-    },
-    servers: [
-      {
-        url: "http://localhost:3000",
-      },
-    ],
-  },
-  apis: ["./routes/*.js"],
-};
-
-const specs = swaggerJsdoc(options);
-app.use(
-  "/api-docs",
-  swaggerUi.serve,
-  swaggerUi.setup(specs)
-);
+swaggerConfig(app);
 
 const port = process.env.API_PORT;
 
---
+++ swaggerConfig.js
@@ -0,0 +1,39 @@
+swaggerJsdoc = require("swagger-jsdoc"),
+swaggerUi = require("swagger-ui-express");
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Code Academy Portfolio Project E-Commerce API",
+      version: "0.1.0",
+      description:
+        "This is simple E-Commerce API using the PERN stack",
+      license: {
+        name: "MIT",
+        url: "https://spdx.org/licenses/MIT.html",
+      },
+      contact: {
+        name: "Support",
+        url: "https://example.com",
+        email: "support@example.com",
+      },
+    },
+    servers: [
+      {
+        url: "http://localhost:3000",
+      },
+    ],
+  },
+  apis: ["./routes/*.js"],
+};
+
+const specs = swaggerJsdoc(options);
+
+module.exports = function(app){
+    app.use(
+    "/api-docs",
+    swaggerUi.serve,
+    swaggerUi.setup(specs)
+    );
+}
